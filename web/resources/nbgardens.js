b2588512a86def9d49b548292d5b59fea23fc5ce
aerosatar <aerosatar@gmail.com>
2016-07-15 17:20:10 +0100

    Some fixes

--- web/resources/nbgardens.js
+++ web/resources/nbgardens.js
@@ -1,37 +1,3 @@
-// Script to open and close the sidenav
-function w3_open() {
-	document.getElementsByClassName("w3-sidenav")[0].style.display = "block";
-	document.getElementsByClassName("w3-overlay")[0].style.display = "block";
-}
-function w3_close() {
-	document.getElementsByClassName("w3-sidenav")[0].style.display = "none";
-	document.getElementsByClassName("w3-overlay")[0].style.display = "none";
-}
-
-// Used to toggle the menu on small screens when clicking on the menu button
-function myFunction() {
-	var x = document.getElementById("navSmall");
-	if (x.className.indexOf("w3-show") == -1) {
-		x.className += " w3-show";
-	} else {
-		x.className = x.className.replace(" w3-show", "");
-	}
-}
-
-//Used to enable accordion style menu collapse
-function accordionFunction(id) {
-    var x = document.getElementById(id);
-   
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-        x.previousElementSibling.className += " w3-theme-l2";
-    } else { 
-        x.className = x.className.replace("w3-show", "");
-        x.previousElementSibling.className = 
-        x.previousElementSibling.className.replace(" w3-theme-l2", "");
-    }
-}
-
 // Used to automatically generate navigation breadcrumbs
 function breadcrumbs() {
   sURL = new String;
